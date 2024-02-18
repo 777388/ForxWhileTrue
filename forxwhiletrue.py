9f99f5a7a726a4070e7f9065735bdde65a093b39
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-02-18 12:07:16 -0800

    Update forxwhiletrue.py

--- forxwhiletrue.py
+++ forxwhiletrue.py
@@ -1,19 +1,24 @@
 import sys
+import os
 class forxwhileTrue:
     def __init__(self, x=0, boolean=True):
         self.x = x
         self.boolean = boolean
-        while boolean:
-            #enter the program here with conditionals set to change the Boolean value at triggers
+    def run(self):
+        while self.boolean:
+            os.popen(**self.kwargs)
             self.x += 1
             continue
 class forx(forxwhileTrue):
     def __init__(self):
         super(forx, self).__init__()
-def forxx(self, kwargs):
+
+def forxx(self, **kwargs):
     self.kwargs = kwargs
     {lambda: forx
         .type(forxwhileTrue)
         .value(**self.kwargs)
         .trigger(False)
     }
+
+forxx.run(sys.argv[1:])
